777b3a6b426c3246af0ce3ddff17482d3d0f828c
Jackrim1 <jriminton@gmail.com>
2017-05-02 20:15:59 +0100

    record created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :records
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'records#index'
 end
